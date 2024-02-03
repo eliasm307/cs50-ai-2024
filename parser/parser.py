9810c05d65db274914a8ab5a29824d0f3f4e7026
Eli <13420359+eliasm307@users.noreply.github.com>
2024-02-03 19:14:41 +0000

    parser passing tests, looking into edge cases from the project description

---
+++ parser/parser.py
@@ -0,0 +1,155 @@
+from re import split
+import nltk
+import sys
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and" | "until"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+NONTERMINALS = """
+S -> NP VP | NP P NP | NP VP Det NP | S Conj S | NP VP P NP | S Conj VP NP
+NP -> N | Det NP | NP Adv | NP P NP | Adj NP
+VP -> V | Adv VP | VP Adv | VP P
+"""
+
+"""
+Examples:
+
+Text: She never said a word until we were at the door here.
+Symbols: N Adv V Det N Conj N V P Det N Adv
+Sentence structure: (NP VP NP) Conj (NP VP Det NP) = S Conj S
+
+Text: holmes sat in the red armchair and he chuckled
+Symbols: N V P Det Adj N Conj N V
+Sentence structure: (NP VP Det NP) Conj (NP VP) = S Conj S
+
+Text: holmes chuckled to himself
+Symbols: N V P N
+Sentence structure: NP VP P NP
+
+Text: holmes sat down and lit his pipe
+Symbols: N V Adv Conj V Det N
+Sentence structure: (NP VP) Conj VP NP = S Conj VP NP
+
+
+"""
+
+
+# CFG - Context Free Grammar
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+
+def get_symbols(token: str):
+    return [p.lhs().symbol() for p in grammar.productions(None, token)]
+
+
+def print_sentence_debug(tokens: list[str]):
+    print("Chart:", parser.chart_parse(tokens).pretty_format())
+    print("\nText:", *tokens)
+    all_symbols = []
+    for token in tokens:
+        token_symbols = get_symbols(token)
+        all_symbols += token_symbols
+        print(token, ":", *token_symbols)
+
+    print("\nSymbols:", *all_symbols)
+
+
+def main():
+    # If filename specified, read sentence from file
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            s = f.read()
+
+    # Otherwise, get sentence as input
+    else:
+        s = input("Sentence: ")
+
+    # Convert input into list of words
+    s = preprocess(s)
+
+    # Attempt to parse sentence
+    try:
+        trees = list(parser.parse(s))
+    except ValueError as e:
+        print(e)
+        return
+
+    if not trees:
+        print("Could not parse sentence.")
+        print_sentence_debug(s)
+        return
+
+    # Print each tree with noun phrase chunks
+    for tree in trees:
+        tree.pretty_print()
+
+        print("Noun Phrase Chunks")
+        for np in np_chunk(tree):
+            print(" ".join(np.flatten()))
+
+
+def contains_some_alpha_chars(word: str):
+    return any(c.isalpha() for c in word.strip())
+
+
+def preprocess(sentence: str):
+    """
+    Convert `sentence` to a list of its words.
+    Pre-process sentence by converting all characters to lowercase
+    and removing any word that does not contain at least one alphabetic
+    character.
+    """
+    return [
+        word
+        for word in nltk.word_tokenize(sentence.lower())
+        if contains_some_alpha_chars(word)
+    ]
+
+
+def is_np_tree(tree: nltk.Tree) -> bool:
+    return tree.label() == "NP"
+
+
+def is_np_chunk(tree: nltk.Tree) -> bool:
+    if not is_np_tree(tree):
+        return False
+
+    for subtree in tree.subtrees():
+        if tree == subtree:
+            continue  # NOTE: subtrees includes the root tree
+
+        if is_np_tree(subtree):
+            return False  # NP tree contains NP subtrees
+
+    return True  # NP tree does not contain NP subtrees
+
+
+def np_chunk(tree: nltk.Tree):
+    """
+    Return a list of all noun phrase chunks in the sentence tree.
+    A noun phrase chunk is defined as any subtree of the sentence
+    whose label is "NP" that does not itself contain any other
+    noun phrases as subtrees.
+    """
+    chunks = [
+        np_subtree
+        for np_subtree in tree.subtrees(is_np_tree)
+        if is_np_chunk(np_subtree)
+    ]
+
+    return chunks
+
+
+if __name__ == "__main__":
+    main()
